19c20e63d42e6d354b9198a3883a751c9a119d9b
Henry Harris <henrythomasharris@gmail.com>
2019-08-23 19:21:28 -0400

    Add website to cors

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(
   cors({
-    origin: 'http://localhost',
+    origin: ['http://localhost', 'https://henryharr.is'],
   }),
 )
 
