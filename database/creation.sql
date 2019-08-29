3b1fdced6df4b43e6f219c6d6fa250723ffb2154
Henry Harris <henrythomasharris@gmail.com>
2019-08-28 21:52:41 -0400

    Add creator to messages

--- database/creation.sql
+++ database/creation.sql
@@ -3,7 +3,7 @@ CREATE TABLE contact (
   `name` VARCHAR(256) NOT NULL,
   `phone_number` VARCHAR(256) NOT NULL,
 	`created_at` DATETIME(3) NOT NULL,
-	`deleted_at` DATETIME(3),
+	`deleted_at` DATETIME(3) DEFAULT NULL,
 	PRIMARY KEY (id)
 );
 
@@ -13,14 +13,16 @@ CREATE TABLE user (
   `password` VARCHAR(256) NOT NULL,
   `email` VARCHAR(256),
 	`created_at` DATETIME(3) NOT NULL,
-	`deleted_at` DATETIME(3),
+	`deleted_at` DATETIME(3) DEFAULT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE message (
 	`id` INT NOT NULL AUTO_INCREMENT,
   `content` VARCHAR(160) NOT NULL,
+  `created_by` INT NOT NULL,
 	`created_at` DATETIME(3) NOT NULL,
-	`deleted_at` DATETIME(3),
+	`deleted_at` DATETIME(3) DEFAULT NULL,
+  CONSTRAINT fk_creator FOREIGN KEY (created_by) REFERENCES user(id) ON UPDATE CASCADE, 
 	PRIMARY KEY (id)
 );
